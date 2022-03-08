cf3783110ea07f61e9888a620094b55a8b826a55
Benkedek <benkedek@gmail.com>
2022-03-08 21:33:44 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './style.css';
-import { createStore } from 'redux';
-const valami = createStore(
-  (state, action) => {
-    console.log(state, action);
-    return { actionType: 'test' };
-  },
-  { actionType: 'def' }
-);
+import { createStore, combineReducers } from 'redux';
+
+let initState = {
+  myStateProp: 'def',
+};
+const reducer = (state, action) => {
+  console.log(state, action);
+  return { actionType: 'test' };
+};
+const reducer2 = (state, action) => {
+  console.log(state, action);
+  return { actionType: 'test2' };
+};
+const combinedReducer = combineReducers({ reducer, reducer2 });
+const valami = createStore(combinedReducer);
 export default function App() {
   return <div>Test</div>;
 }
